e7e6e736da3fa3e59a8e45d36f0f3cb62fcfa52b
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2024-06-15 20:29:51 -0300

    Install kitty systemwide

--- configuration.nix
+++ configuration.nix
@@ -160,6 +160,7 @@
   environment.systemPackages = [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      pkgs.distrobox
+     pkgs.kitty
      pkgs.localsend
      pkgs.vim
      pkgs.virt-viewer
